f2d082665f0ded06671e4c487ba3061a06d9fb7b
pratham meena <yashmeena274@gmail.com>
2024-06-11 20:40:08 +0530

    done something

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className='h-full w-[20%] bg-white p-6 rounded-lg'>
-        <Link to='/'>Home</Link> <br />
-        <Link to='/addContact'>Add Contact</Link>
+        <NavLink to='/' className={(e)=> e.isActive ? 'text-xl font-bold': 'text-xl font-medium'}> <i className="ri-home-fill"></i>  Home</NavLink> <br />
+        <NavLink to='/addContact' className={(e)=> e.isActive ? 'text-xl font-bold': 'text-xl font-medium'} >Add Contact</NavLink>
     </nav>
   )
 }
